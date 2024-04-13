9a1b8623e0979bab40002a90bad55b93c2a9671f
jaeheelee <124273674+jaehee222@users.noreply.github.com>
2024-04-13 18:55:43 +0900

    Create start.js
    진입창

---
+++ src/start.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Text,View, Image, StyleSheet, Dimensions } from 'react-native';
+
+const screenWidth = Dimensions.get('window').width;
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image
+          style={styles.mainImage}
+          source={require('../public/png/free-icon-ticket-389801.png')}
+        />
+      </View>
+    <Text>직관일기장</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainImage: {
+    width: screenWidth - 250,
+    height: screenWidth - 250,
+    resizeMode: 'contain',
+  },
+});
+
+export default App;
